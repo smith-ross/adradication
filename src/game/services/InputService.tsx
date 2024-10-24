5276c7854b256a3a39aeb41a1821459936d87f5a
Ross Smith <smithross2003@gmail.com>
2024-10-24 20:21:27 +0100

    fix: Prevent movement when focused on textbox

--- src/game/services/InputService.tsx
+++ src/game/services/InputService.tsx
@@ -7,11 +7,13 @@ export default class InputService {
   static init() {
     if (InputService.#initialised) return;
     document.body.addEventListener("keydown", (event: KeyboardEvent) => {
+      if (event.target !== document.body) return;
       if (PREVENT_DEFAULTS.includes(event.key)) event.preventDefault();
       InputService.#pressedKeys[event.key.toLowerCase()] = true;
     });
     document.body.addEventListener("keyup", (event: KeyboardEvent) => {
-      if (PREVENT_DEFAULTS.includes(event.key)) event.preventDefault();
+      if (PREVENT_DEFAULTS.includes(event.key) && event.target == document.body)
+        event.preventDefault();
       InputService.#pressedKeys[event.key.toLowerCase()] = false;
     });
     InputService.#initialised = true;
